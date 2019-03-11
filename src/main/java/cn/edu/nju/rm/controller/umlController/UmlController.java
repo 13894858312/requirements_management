33f93c5ce939277af026594b2ae1350b6e608ac0
nananer7 <nananer7@163.com>
2019-03-11 13:18:00 +0800

    细枝末节

---
+++ src/main/java/cn/edu/nju/rm/controller/umlController/UmlController.java
@@ -0,0 +1,25 @@
+package cn.edu.nju.rm.controller.umlController;
+
+import cn.edu.nju.rm.service.UML.UMLService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Author: nananer7
+ * @Description:
+ * @Date: 2019/3/11 12:52
+ */
+
+@Controller
+public class UmlController {
+
+    @Autowired
+    UMLService umlService;
+
+    @RequestMapping(value = "/uml", method = RequestMethod.GET)
+    public String editUML() {
+        return "editUML";
+    }
+}
